7362e2102d09b4a9b0b69923b1378e889e2ae9a1
Thomas Nanchen <thomasnanchen@hotmail.com>
2025-05-16 17:56:45 +0200

    feat(main): add api doc with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Fuel Stations API')
+    .setVersion(process.env.API_VERSION ?? 'API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
